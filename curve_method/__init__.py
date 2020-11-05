0669cd79ea92e9b2f99d918b00a24e57b2a4c383
Alex Freund <alexboarded@gmail.com>
2020-11-05 05:26:47 -0500

    Update module location and install declarations

--- curve_method/__init__.py
+++ curve_method/__init__.py
@@ -1,6 +1,7 @@
-from .version import __version__
 from .evaluation import curve_scores, true_k
 from .plotting import scatter, polyfit
+
+__version__ = '0.1.0'
 
 __all__ = [
     'curve_scores',
--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
+import pathlib
 from setuptools import setup
 
-setup()
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="curve_method",
+    version="0.1.0",
+    description="A quantitative approach to select the optimal number of clusters",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alexjfreund/curve-method",
+    author="Alex Freund",
+    author_email="alexjfreund@gmail.com",
+    licence="MIT",
+    classifiers=[
+        "Licence :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7"
+    ],
+    packages=["curve_method"],
+    include_package_data=True,
+    install_requires=["numpy", "matplotlib", "sklearn"]
+)
