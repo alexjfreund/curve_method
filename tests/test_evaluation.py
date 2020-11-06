055c56ab3a3295d508d9f4bb759b2b5be9a7e0c5
Alex Freund <alexboarded@gmail.com>
2020-11-05 19:39:00 -0500

    Add tests for evaluation return formats

---
+++ tests/test_evaluation.py
@@ -0,0 +1,41 @@
+import unittest
+from sklearn.datasets import make_blobs
+from curve_method import curve_scores, true_k
+class TestEvaluation(unittest.TestCase):
+
+    # validate return format of curve_scores for n=100, features=2, centers=4
+    def small_curve_scores(self):
+        blobs, _ = make_blobs(n_samples=100, n_features=2, centers=4)
+        scores = curve_scores(blobs, k_max=12)
+        self.assertEqual(len(scores), 11)
+        for k in range(2, 13):
+            self.assertTrue(k in scores)
+            self.assertTrue(isinstance(float, scores[k]))
+    
+    # validate return format of curve_scores for n=200, features=8, centers=12
+    def big_curve_scores(self):
+        blobs, _ = make_blobs(n_samples=200, n_features=8, centers=12)
+        scores = curve_scores(blobs, k_max=18)
+        self.assertEqual(len(scores), 17)
+        for k in range(2, 19):
+            self.assertTrue(k in scores)
+            self.assertTrue(isinstance(float, scores[k]))
+    
+    # validate return format of true_k for n=100, features=2, centers=4
+    def small_true_k(self):
+        blobs, _ = make_blobs(n_samples=100, n_features=2, centers=4)
+        k = true_k(blobs, k_max=12)
+        self.assertTrue(isinstance(k, int))
+        self.assertTrue(k >= 2)
+        self.assertTrue(k <= 12)
+    
+    # validate return format of true_k for n=200, features=8, centers=12
+    def big_true_k(self):
+        blobs, _ = make_blobs(n_samples=200, n_features=8, centers=12)
+        k = true_k(blobs, k_max=18)
+        self.assertTrue(isinstance(k, int))
+        self.assertTrue(k >= 2)
+        self.assertTrue(k <= 18)
+
+if __name__ == '__main__':
+    unittest.main()
