a1611b35c19272533c62c4aaaa5ef43be7c3add0
Alex Freund <alexboarded@gmail.com>
2020-11-11 19:16:43 -0500

    Finish unit tests for curve_scores and true_k

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -4,25 +4,25 @@ from curve_method import curve_scores, true_k
 class TestEvaluation(unittest.TestCase):
 
     # validate return format of curve_scores for n=100, features=2, centers=4
-    def small_curve_scores(self):
+    def test_small_curve(self):
         blobs, _ = make_blobs(n_samples=100, n_features=2, centers=4)
         scores = curve_scores(blobs, k_max=12)
         self.assertEqual(len(scores), 11)
         for k in range(2, 13):
             self.assertTrue(k in scores)
-            self.assertTrue(isinstance(float, scores[k]))
+            self.assertTrue(isinstance(scores[k], float))
     
     # validate return format of curve_scores for n=200, features=8, centers=12
-    def big_curve_scores(self):
+    def test_big_curve(self):
         blobs, _ = make_blobs(n_samples=200, n_features=8, centers=12)
         scores = curve_scores(blobs, k_max=18)
         self.assertEqual(len(scores), 17)
         for k in range(2, 19):
             self.assertTrue(k in scores)
-            self.assertTrue(isinstance(float, scores[k]))
+            self.assertTrue(isinstance(scores[k], float))
     
     # validate return format of true_k for n=100, features=2, centers=4
-    def small_true_k(self):
+    def test_small_k(self):
         blobs, _ = make_blobs(n_samples=100, n_features=2, centers=4)
         k = true_k(blobs, k_max=12)
         self.assertTrue(isinstance(k, int))
@@ -30,7 +30,7 @@ class TestEvaluation(unittest.TestCase):
         self.assertTrue(k <= 12)
     
     # validate return format of true_k for n=200, features=8, centers=12
-    def big_true_k(self):
+    def test_big_k(self):
         blobs, _ = make_blobs(n_samples=200, n_features=8, centers=12)
         k = true_k(blobs, k_max=18)
         self.assertTrue(isinstance(k, int))
